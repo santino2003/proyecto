6671e1e8fa818f703ca25dbb933fff04c1912774
santinopeiretti <santinopeiretti2003@gmail.com>
2023-09-04 18:43:40 -0300

    SimulacionJsd

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,7 +16,9 @@ class Prodcuto{
         }
     }
 
-    
+    devolverProducto(){
+        this.cantidadDisponible += 1
+    }
 
 }
 
@@ -38,8 +40,10 @@ class Carrito{
         for (let i = 0; i< this.elementos.length; i++){
             if ((this.elementos[i][nombreProducto]) == prodcuto){
                 this.elementos.splice(i,1)
+                return true
             }
         }
+        return false
 
     }
     vaciarCarrito(){
@@ -55,3 +59,142 @@ class Carrito{
         return valor
     }   
 }
+
+let RemeraNegra = new Prodcuto('RemeraNegra', 9000, 0)
+let RemeraBlanca = new Prodcuto('RemeraBlanca', 6000, 3)
+let RemeraRoja = new Prodcuto('RemeraRoja', 4000, 2)
+
+let carrito = new Carrito()
+
+function consolaVenderProducto(producto){
+    if (producto == 'RemeraNegra'){
+        unidad = RemeraNegra.venderPrducto()
+        if (unidad != undefined){
+            carrito.agregarProducto(unidad)
+            console.log('Agregado exitosamente')
+        }
+        else{
+            console.log("producto sin stock")
+        }
+    }
+    else if (producto == 'RemeraBlanca'){
+        unidad = RemeraBlanca.venderPrducto()
+        if (unidad != undefined){
+            carrito.agregarProducto(unidad)
+            console.log('Agregado exitosamente')
+        }
+        else{
+            console.log("producto sin stock")
+        }
+    }
+    else if (producto == 'RemeraRoja'){
+        unidad = RemeraRoja.venderPrducto()
+        if (unidad != undefined){
+            carrito.agregarProducto(unidad)
+            console.log('Agregado exitosamente')
+            }
+        else{
+            console.log("producto sin stock")
+        }
+    }
+    else{
+        console.log('producto invalido')
+    }
+}
+function consolaEliminarProducto(producto){
+    if (producto == 'RemeraNegra'){
+        elimineProducto = carrito.eliminarUnProducto(producto)
+        if (elimineProducto){
+            RemeraNegra.devolverProducto()
+            console.log('Eliminado Exitosamente')
+        }
+        else{
+            console.log("El producto no se encuentra en el carrito")
+        }
+    }
+    else if (producto == 'RemeraBlanca'){
+        elimineProducto = carrito.eliminarUnProducto(producto)
+        if (elimineProducto){
+            RemeraBlanca.devolverProducto()
+            console.log('Eliminado Exitosamente')
+        }
+        else{
+            console.log("El producto no se encuentra en el carrito")
+        }
+    }
+    else if (producto == 'RemeraRoja'){
+        elimineProducto = carrito.eliminarUnProducto(producto)
+        if (elimineProducto){
+            RemeraRoja.devolverProducto()
+            console.log('Eliminado Exitosamente')
+        }
+        else{
+            console.log("El producto no se encuentra en el carrito")
+        }
+    }
+    else{
+        console.log('producto invalido')
+    }
+}
+
+function escribir(){
+    console.log('Bienvenidos a MoonShop, nuestros prodcutos en stock son: RemeraNegra, RemeraBlanca, RemeraRoja')
+    console.log('escribe "agregarProducto nombreProducto" para agregar el producto al carrito')
+    console.log('escribe "eliminarProducto nombreProducto" para eliminar el producto al carrito')
+    console.log('escribe "vaciarCarrito" para dejar el carrito en cero')
+    console.log('escribe "finalizarCompra" para finalizar la compra y llevarte nuestros prodcutos de ultima moda')
+    console.log('escribe "exit" para irte de nuestra tienda sin comprar')
+}
+
+
+function main(){
+    let termine = false
+    while (! termine){
+        let ingresado = prompt('Que desea hacer')
+        arreglo = ingresado.split(" ")
+        comando = arreglo[0]
+        if (comando == 'agregarProducto'){
+            if (arreglo.length != 2){
+                console.log('falta agregar producto para sumar al carrito')
+            }
+            else {
+                consolaVenderProducto(arreglo[1])
+            }
+        }
+        else if (comando == 'eliminarProducto'){
+            if (arreglo.length != 2){
+                console.log('falta agregar producto a eliminar')
+            }
+            else {
+                consolaEliminarProducto(arreglo[1])
+            }
+        }
+        else if (comando == 'vaciarCarrito'){
+            carrito.vaciarCarrito()
+            console.log('carrito vaciado con exito')
+        }
+        else if (comando == 'finalizarCompra'){
+            valorAAbonar = carrito.valorTotal()
+            console.log('Su total a abonar es $' + valorAAbonar)
+            console.log('Muchas gracias por su compra')
+            termine = true
+        }
+        else if (comando == 'exit'){
+            termine = true
+            console.log('Gracias por su visita')
+        }
+        else{
+            console.log('comando incorrecto')
+        }
+        
+    
+    }
+}
+
+setTimeout(() => {
+    escribir()
+}, 2000);
+
+setTimeout(() => {
+    main()
+}, 3000);
